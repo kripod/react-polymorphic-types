b2838f930e5bd83cf069f745422bf19467b660a0
Kristóf Poduszló <kripod@protonmail.com>
2021-07-11 19:06:36 +0200

    feat!: type inference for event parameters
    fixes #8

--- index.d.ts
+++ index.d.ts
@@ -8,12 +8,16 @@ type Merge<T, U> = Omit<T, keyof U> & U;
 type PropsWithAs<P, T extends React.ElementType> = P & { as?: T };
 
 export type PolymorphicPropsWithoutRef<P, T extends React.ElementType> = Merge<
-	React.ComponentPropsWithoutRef<T>,
+	T extends keyof JSX.IntrinsicElements
+		? React.PropsWithoutRef<JSX.IntrinsicElements[T]>
+		: React.ComponentPropsWithoutRef<T>,
 	PropsWithAs<P, T>
 >;
 
 export type PolymorphicPropsWithRef<P, T extends React.ElementType> = Merge<
-	React.ComponentPropsWithRef<T>,
+	T extends keyof JSX.IntrinsicElements
+		? React.PropsWithRef<JSX.IntrinsicElements[T]>
+		: React.ComponentPropsWithRef<T>,
 	PropsWithAs<P, T>
 >;
 
